c41a6d56d191c2da02eb4c7f66628c24b4d86a71
vidiSEO <matt@vidiseo.com>
2013-07-17 09:15:17 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'e869d3b8f746004f1c116e4650e5b6308968caf110dd310eadc517e55794d382dcbaf90f4c7edbc4b32983980f8af8a201ca3924bcb58c7d507915da9e5fdbfd'
